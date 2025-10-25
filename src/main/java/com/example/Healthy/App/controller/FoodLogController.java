94aa5b199e2d6db930ca7674d9ae301b3374198b
LanCute2212 <phongvan1908@gmail.com>
2025-10-25 23:40:56 +0700

    update

--- src/main/java/com/example/Healthy/App/controller/FoodLogController.java
+++ src/main/java/com/example/Healthy/App/controller/FoodLogController.java
@@ -2,7 +2,6 @@ package com.example.Healthy.App.controller;
 
 import com.example.Healthy.App.dto.FoodLogDto;
 import com.example.Healthy.App.dto.response.BaseResponse;
-import com.example.Healthy.App.model.Status;
 import com.example.Healthy.App.service.FoodLogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -18,18 +17,6 @@ public class FoodLogController {
     public FoodLogController(FoodLogService foodLogService) {
         this.foodLogService = foodLogService;
     }
-
-    @PostMapping
-    public BaseResponse<FoodLogDto> createFoodLog(@RequestBody FoodLogDto foodLogDto) {
-        FoodLogDto createdFoodLog = foodLogService.createFoodLog(foodLogDto);
-        return BaseResponse.<FoodLogDto>builder()
-                .status(HttpStatus.CREATED.value())
-                .error(false)
-                .message("Food log created")
-                .data(createdFoodLog)
-                .build();
-    }
-
     @GetMapping
     public BaseResponse<List<FoodLogDto>> getAllFoodLogs() {
         List<FoodLogDto> foodLogs = foodLogService.getAllFoodLogs();
@@ -40,7 +27,6 @@ public class FoodLogController {
                 .data(foodLogs)
                 .build();
     }
-
     @GetMapping("/{id}")
     public BaseResponse<FoodLogDto> getFoodLogById(@PathVariable Integer id) {
         FoodLogDto foodLog = foodLogService.getFoodLogByID(id);
@@ -49,27 +35,6 @@ public class FoodLogController {
                 .error(false)
                 .message(".")
                 .data(foodLog)
-                .build();
-    }
-
-    @PutMapping("/{id}")
-    public BaseResponse<FoodLogDto> updateFoodLog(@PathVariable Integer id, @RequestBody FoodLogDto foodLogDto) {
-        FoodLogDto updatedFoodLog = foodLogService.updateFoodLog(id, foodLogDto);
-        return BaseResponse.<FoodLogDto>builder()
-                .status(Status.UPDATED.getStatus())
-                .error(false)
-                .message(Status.UPDATED.getMessage())
-                .data(updatedFoodLog)
-                .build();
-    }
-
-    @DeleteMapping("/{id}")
-    public BaseResponse deleteFoodLog(@PathVariable Integer id) {
-        foodLogService.deleteFoodLog(id);
-        return BaseResponse.builder()
-                .status(Status.DELETED.getStatus())
-                .error(false)
-                .message(Status.DELETED.getMessage())
                 .build();
     }
 }
